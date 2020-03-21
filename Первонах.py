3f2bf10ae61b50b6989d7d49ace064ee83bd2524
Евгений <61330711+evgenyterentyev56@users.noreply.github.com>
2020-03-22 01:59:29 +0500

    Добавлен файл первонах

---
+++ Первонах.py
@@ -0,0 +1,24 @@
+
+
+from colorama import init
+from colorama import Fore, Back, Style
+init()
+
+print(Back.YELLOW)
+
+what = input("Что делаем? (+,-): " )
+
+print(Back.GREEN)
+
+a = float(input("Введи первое число"))
+b = float(input("Введи второе число"))
+#float - мы перебрасываем это число в ВЕЩЕСТВЕННОЕ
+#if - ЭТО ЕСЛИ
+if what == "+":
+    c = a + b
+    print("Результат: " + str(c))
+elif what == "-":
+    c = a - b
+    print("Результат: " + str(c))
+else:
+    print("Выполнена неверная операция!")
